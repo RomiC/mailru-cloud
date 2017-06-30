15feae87c6e6f90566d424e3dd8c5dd3ffffcf74
Roman Charugin <roman_charugin@epam.com>
2017-06-30 18:30:06 +0300

    Authorization process

--- examples/auth.js
+++ examples/auth.js
@@ -1,52 +1,86 @@
 const FormData = require('form-data');
-const { request } = require('https');
-const { parse: parseUrl } = require('url');
 
-const { post } = require('../lib/request');
+const request = require('../lib/request');
 
-const req = request(
-  Object.assign({}, parseUrl('https://auth.mail.ru/cgi-bin/auth'), {
-    query: {
-      lang: 'ru_RU',
-      from: 'authpop'
-    },
-    method: 'POST'
+/**
+ * Common auth request
+ * @param {string} login User login
+ * @param {string} password User password
+ * @param {string} domain User email's domain
+ * @return {Promise} Promise
+ */
+const commonAuth = (login, password, domain) => request({
+  url: 'https://auth.mail.ru/cgi-bin/auth',
+  query: {
+    lang: 'ru_RU',
+    from: 'authpop'
+  },
+  method: 'POST',
+  json: false,
+  fullResponse: true
+})
+  .then(({ headers }) => {
+    const cookies = headers['set-cookie'];
+    if (typeof cookies !== 'undefined' && cookies.length > 0) {
+      return cookies;
+    } else {
+      throw new Error('Wrong login or password.');
+    }
+  });
+
+/**
+ * Get sdc-url
+ * @param {Array} cookies Cookies for authentificates user
+ * @return {Promise} Promise
+ */
+const getSdcUrl = (cookies) => request({
+  url: 'https://auth.mail.ru/sdc',
+  query: {
+    from: 'https://cloud.mail.ru/home/'
+  },
+  headers: {
+    Cookie: cookies.filter((cookie) => /domain=(\.auth)?\.mail\.ru/.test(cookie))
+      .map((cookie) => cookie.replace(/^([^;]+);.+$/, '$1'))
+      .join('; ')
+  },
+  json: false,
+  fullResponse: true
+})
+  .then(({ statusCode, headers }) => {
+    if (statusCode === 302 && typeof headers['Location'] !== 'undefined') {
+      return {
+        cookies,
+        sdcUrl: headers['Location']
+      };
+    } else {
+      throw new Error('SDC-Url is empty!');
+    }
+  });
+
+/**
+ * Get sdc-token
+ * @param {object} options Object with cookies and sdcurl to follow
+ * @param {Array} options.cookies Array with cookies
+ * @param {string} options.sdcUrl SDC-Url to follow
+ * @return {Promise} Promise
+ */
+const getSdcToken = ({ cookies, sdcUrl }) => request({
+  url: sdcUrl,
+  headers: {
+    Cookie: cookies.filter((cookie) => /domain=(\.cloud)?\.mail\.ru/.test(cookie))
+      .map((cookie) => cookie.replace(/^([^;]+);.+$/, '$1'))
+      .join('; ')
+  },
+  json: false,
+  fullResponse: true
+})
+  .then(({ headers }) => {
+    const cookies = headers['set-cookie'];
+    if (typeof cookies !== 'undefined' && cookies.lenght > 0) {
+
+    }
   })
-);
 
-req.on('response', (res) => {
-  const cookies = res.headers['set-cookie'];
-  if (cookies !== 'undefined' && cookies.lenght > 0) {
-    const req = request(
-      Object.assign({}, parseUrl('https://auth.mail.ru/sdc'), {
-        query: {
-          from: 'https://cloud.mail.ru/home/'
-        },
-        headers: {
-          Cookie: cookies.filter((cookie) => /domain=(\.auth)?\.mail\.ru/.test(cookie))
-            .map((cookie) => cookie.replace(/^([^;]+);.+$/, '$1'))
-            .join('; ')
-        }
-      })
-    );
-  } else {
-    throw new Error('auth failed');
-  }
-});
-
-req.on('error', (err) => {
-  err;
-});
-
-// const form = new FormData();
-// form.append('page', 'https://cloud.mail.ru/?from=promo');
-// form.append('FailPage', '');
-// form.append('Domain', 'mail.ru');
-// form.append('Login', 'roman.charugin');
-// form.append('Password', 'genesis');
-// form.append('new_auth_form', 1);
-// form.append('saveauth', 1);
-
-// form.pipe(req);
-
-req.end('Domain=mail.ru&Login=roman.charugin&Password=genesis');
+module.exports = (login, password, domain) => commonAuth(login, password, domain)
+  .then(getSdcUrl)
+  .then(getSdcToken);
