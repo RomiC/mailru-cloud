b68f2e9a8c30fb3b6ff86e00777023938632f041
Roman Charugin <roman_charugin@epam.com>
2017-07-13 17:09:22 +0300

    Complete with file uploading
    Added auth function;
    Added user.space method;
    Added file.add and file.upload methods;
    Wrote examples for methods above;
    Updated API docs according auth and file uploading;
    Move api docs to separate file

--- examples/file.js
+++ examples/file.js
@@ -1,5 +1,15 @@
-const path = require('path');
+const { default: auth } = require('../lib/auth');
+const { upload, add } = require('../lib/file');
 
-const file = require('../lib/file');
+const file = './image.jpg';
 
-file.add(path.join(__dirname, 'file.js'));
+auth('roman.charugin', 'genesis', 'mail.ru')
+  .then((auth) => upload(auth, file)
+    .then(({ hash, size }) => add(auth, {
+      name: '//image.jpg',
+      hash,
+      size
+    })))
+  .then((res) => {
+    res;
+  });
