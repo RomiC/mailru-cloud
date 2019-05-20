c5a05d39aee86eab2d4f3564b09698bdf9ac7616
Roman Charugin <i@charugin.ru>
2019-05-20 13:12:48 +0300

    DEbug #2

--- src/auth.ts
+++ src/auth.ts
@@ -68,7 +68,7 @@ async function getSdcUrl(context: IAuthRequestContext): Promise<IAuthRequestCont
   const location = info.headers['location'];
 
   if (info.statusCode !== 302 || !/token=[^\&]+/.test(location)) {
-    console.log(info);
+    console.log(info.statusCode, info.headers);
     throw new Error('Failed to get SDC-url');
   }
 
