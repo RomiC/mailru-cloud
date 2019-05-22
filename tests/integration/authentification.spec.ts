34427697144ae6fc9bd834f95cb4385be9516f26
Roman Charugin <i@charugin.ru>
2019-05-22 11:12:05 +0300

    Returned back test case for the authentification

--- tests/integration/authentification.spec.ts
+++ tests/integration/authentification.spec.ts
@@ -9,9 +9,9 @@ if (process.env.TRAVIS !== 'true') {
 
 const { MAILRU_LOGIN, MAILRU_PASSWORD } = process.env;
 
-// test('should throw an error in case of wrong login or password', () => {
-//   return expect(auth('bad_login', 'bad_password', 'mail.ru')).rejects.toEqual(new Error('Wrong login or password'));
-// });
+test('should throw an error in case of wrong login or password', () => {
+  return expect(auth('bad_login', 'bad_password', 'mail.ru')).rejects.toEqual(new Error('Wrong login or password'));
+});
 
 test('should authentificate user with valid login and password', () => {
   return auth(MAILRU_LOGIN, MAILRU_PASSWORD, MAILRU_DOMAIN).then((res) => {
